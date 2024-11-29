1744f5569d70ca37a8fd6627b408c010f91c23de
rberenga <rberenga@dip149.ad.studenti.math.unipd.it>
2024-11-29 15:46:44 +0100

    creazione e popolazione corrette e compilate

--- creazionetabelle.sql
+++ creazionetabelle.sql
@@ -13,10 +13,10 @@ DROP TABLE IF EXISTS Lista_operazioni CASCADE;
 DROP TABLE IF EXISTS Lista_farmaci CASCADE;
 
 CREATE TABLE IF NOT EXISTS Reparti(
-    nome_reparto VARCHAR(16) PRIMARY KEY,
+    nome_reparto VARCHAR(32) PRIMARY KEY,
     piano INT NOT NULL,
     capacita_massima INT NOT NULL,
-    telefono_reparto  VARCHAR(10) NOT NULL,
+    telefono_reparto  VARCHAR(16) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Personale_medico(
@@ -28,23 +28,22 @@ CREATE TABLE IF NOT EXISTS Personale_medico(
     comune_nascita VARCHAR(32) NOT NULL,
     stipendio INT NOT NULL,
     capo_reparto BOOLEAN,
-    reparto VARCHAR(16) NOT NULL,
+    reparto VARCHAR(32) NOT NULL,
     FOREIGN KEY (reparto) REFERENCES Reparti(nome_reparto) 
 );
-
 
 CREATE TABLE IF NOT EXISTS Pazienti(
     c_f VARCHAR(16) PRIMARY KEY,
     nome VARCHAR(32) NOT NULL,
     cognome VARCHAR(32) NOT NULL,
-    sesso VARCHAR(8) NOT NULL,
     data_nascita DATE NOT NULL,
+    sesso VARCHAR(8) NOT NULL,
     comune_nascita VARCHAR(32) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Camere(
     id_camera INT PRIMARY KEY,
-    nome_reparto VARCHAR(16) NOT NULL,
+    nome_reparto VARCHAR(32) NOT NULL,
     massimo_letti INT NOT NULL,
     FOREIGN KEY (nome_reparto) REFERENCES Reparti(nome_reparto)
 );
@@ -52,10 +51,10 @@ CREATE TABLE IF NOT EXISTS Camere(
 CREATE TABLE IF NOT EXISTS Ricoveri(
     id_ricovero INT PRIMARY KEY,
     data_ricovero DATE NOT NULL,
-    ora_ricovero INT NOT NULL,
-    stato_ricovero VARCHAR(16) NOT NULL,
+    ora_ricovero TIME NOT NULL,
+    stato_ricovero VARCHAR(32) NOT NULL,
     id_camera INT NOT NULL,
-    cf_ricoverato INT NOT NULL,
+    cf_ricoverato VARCHAR(16) NOT NULL,
     FOREIGN KEY (cf_ricoverato) REFERENCES Pazienti(c_f)
 );
 
@@ -78,14 +77,13 @@ CREATE TABLE IF NOT EXISTS Cartella_clinica(
     patologie VARCHAR(64),
     gruppo_sanguigno GRUPPO NOT NULL,
     cf_paziente VARCHAR(16) NOT NULL,
-    id_cura INT NOT NULL,
     FOREIGN KEY (cf_paziente) REFERENCES Pazienti(c_f)
 );
 
 CREATE TABLE IF NOT EXISTS Sale_operatorie(
     id_sala INT PRIMARY KEY,
     max_persone INT NOT NULL,
-    livello_attrezzatura INT NOT NULL
+    livello_attrezzatura VARCHAR(16) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Operazioni(
@@ -104,9 +102,8 @@ CREATE TABLE IF NOT EXISTS Farmaci(
     id_farmaco INT PRIMARY KEY,
     nome VARCHAR(32) NOT NULL,
     dosaggio VARCHAR(32) NOT NULL,
-    effetti VARCHAR(64) NOT NULL,
     controindicazioni VARCHAR(64) NOT NULL,
-    data_scandenza DATE NOT NULL,
+    data_scadenza DATE NOT NULL,
     allergeni VARCHAR(64)
 );
 
@@ -114,9 +111,6 @@ CREATE TABLE IF NOT EXISTS Cure(
     id_cura INT PRIMARY KEY,
     badge INT NOT NULL,
     id_cartella INT NOT NULL,
-    id_farmaco INT NOT NULL,
-    data_ DATE NOT NULL,
-    ora INT NOT NULL,
     FOREIGN KEY (badge) REFERENCES Personale_medico(badge),
     FOREIGN KEY (id_cartella) REFERENCES Cartella_clinica(id_cartella)
 );
